bd762496d6b69716907708dd2efe7ff2ce8807d5
CanGuvercin <mustafacanguvercin@gmail.com>
2025-05-17 22:10:45 +0300

    Project tested, h2 and its web UI works properly, can bu usable for upcaoimng prototypes. PostreSQL is more capable

---
+++ src/main/java/eu/guvercin/BookController.java
@@ -0,0 +1,27 @@
+package eu.guvercin;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping
+    public List<Book> getAllBooks (){
+        return bookService.getAllBooks();
+    }
+
+    @PostMapping
+    public Book createBook(@RequestBody Book book){
+        return bookService.saveBook(book);
+    }
+
+
+}
---
+++ src/main/java/eu/guvercin/BookRepository.java
@@ -0,0 +1,9 @@
+package eu.guvercin;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book,Long > {
+
+
+
+}
